761372c9a7960e1e7f5e15d9d466f58e5311ec95
Alex <mynameislyosha2412@yandex.com>
2019-05-27 01:06:02 +0300

    task 5 - reverse 7 digit number

---
+++ Lesson4/Task5/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        // 5. Test reverse number
+        num = 1234567;
+        System.out.println(num + " (reverse)-> " + NumberLogic.reverseNumber7(num));	// 7654321 - test OKAY
+    }
+}
