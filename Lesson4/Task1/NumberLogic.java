580359385a2d19f328b9bcc83886a2935503cf60
Alex <mynameislyosha2412@yandex.com>
2019-05-29 10:05:25 +0300

    add functions

--- Lesson4/Task1/NumberLogic.java
+++ Lesson4/Task1/NumberLogic.java
@@ -3,4 +3,8 @@ class NumberLogic {
     public static boolean equalsThreeNumbers(int a, int b, int c) {
         return a == b && a == c;
     }
+	
+    public static boolean notEqualsThreeNumbers(int a, int b, int c) {
+        return a != b && a != c && b != c;
+    }
 }
--- Lesson4/Task3/NumberLogic.java
+++ Lesson4/Task3/NumberLogic.java
@@ -16,4 +16,22 @@ class NumberLogic {
 			
         return n1 > n2 && n2 > n3 && n3 > n4;	// check and return result
     }
+	
+	// 3. Descending numbers?
+    public static boolean checkNumber4(int number) {
+		
+        int n1 = number % 10;	// ...X
+        number /= 10;
+
+        int n2 = number % 10; 	// ..X.
+        number /= 10;
+
+        int n3 = number % 10;	// .X..
+        number /= 10;
+
+        int n4 = number % 10;	// X...
+        number /= 10;
+			
+        return n1 < n2 && n2 < n3 && n3 < n4;	// check and return result
+    }
 }
