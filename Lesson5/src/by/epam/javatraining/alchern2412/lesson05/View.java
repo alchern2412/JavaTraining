94d50f52f62f98aad0b96963f2cf2b9c8d5738b2
Alex <mynameislyosha2412@yandex.com>
2019-06-01 10:24:25 +0300

    add view

---
+++ Lesson5/src/by/epam/javatraining/alchern2412/lesson05/View.java
@@ -0,0 +1,11 @@
+package by.epam.javatraining.alchern2412.lesson05;
+
+/**
+ *
+ * @author Alex
+ */
+public class View {
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+}
